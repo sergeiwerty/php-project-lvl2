0d59304f4398a8468437f927a46587f77bf31d2b
sergeiwerty <dshl.srp@gmail.com>
2022-06-27 17:35:55 +0300

    finiish 6th step

--- src/Differ/Builder.php
+++ src/Differ/Builder.php
@@ -4,7 +4,9 @@ namespace Differ\Differ\Builder;
 
 use function Functional\sort;
 use function Differ\Parsers\Parser\parse;
+//use function Differ\Formatters\JSONFormatter\buildStyledFormat;
 use function Differ\Formatters\JSONFormatter\buildStyledFormat;
+
 
 function buildDiff($firstPath, $secondPath,)
 {
@@ -16,54 +18,47 @@ function buildDiff($firstPath, $secondPath,)
 
     $dataByStrings2 = trim(var_export($parsedNodes2, true), "'");
 
-//    $iter = function($currentValue) use (&$iter) {
-//        if(!is_array($currentValue)) {
-//            print_r($currentValue);
-//            return $currentValue;
-//        }
-//
-//        $children = array_map(
-//            function ($key, $value) use (&$iter) {
-//                var_dump($key, $value);
-//                return [$key => $value, 'children' => $iter($value)];
-//            },
-//            array_keys($currentValue),
-//            $currentValue
-//        );
-//        print_r($children);
-//        return $iter;
-//    };
-
-//    $my = ['nodeList', [['leafNode', 'myKey', 'myValue', 'unchanged'], ['leafNode', 'otherKey', 'otherValue', 'changed']]];
-
-    $my = function($currentNode1, $currentNode2) use (&$my) {
+    $iterAst = function($currentNode1, $currentNode2) use (&$iterAst) {
+//        print_r($currentNode1);
+//        print_r($currentNode2);
         $merged = array_keys(array_merge($currentNode1, $currentNode2));
         $sortedKeys = sort($merged, fn($left, $right) => strcmp($left, $right));
 
-        print_r($currentNode1);
-        print_r($currentNode2);
-
         return array_map(
-            function($key) use (&$my, $currentNode1, $currentNode2) {
+            function($key) use (&$iterAst, $currentNode1, $currentNode2, $sortedKeys) {
                 $result = [];
                 if (!array_key_exists($key, $currentNode1)) {
-                    $nodeType = is_array($currentNode2[$key]) ? 'listNode' : 'leafNode';
-                    return [...$result, $key => ['type' => $nodeType, 'value' => $currentNode2[$key], 'status' => 'added']];
-                } elseif (!array_key_exists($key, $currentNode2)) {
-                    $nodeType = is_array($currentNode1[$key]) ? 'listNode' : 'leafNode';
-                    return [...$result, $key => ['type' => $nodeType, 'value' => $currentNode1[$key], 'status' => 'deleted']];
-                } elseif ($currentNode1[$key] !== $currentNode2[$key]) {
-                    $nodeType = is_array($currentNode2[$key]) ? 'listNode' : 'leafNode';
-                    $state = $nodeType === 'leafNode' ? [$currentNode1[$key], $currentNode2[$key]] : $my($currentNode1[$key], $currentNode2[$key]);
-//                    $result = [...$result, ]
-//                    print_r($currentNode1[$key]);
-//                    print_r($currentNode2[$key]);
-
-                    return [...$result, $key => ['type' => $nodeType, 'value' => $state, 'status' => 'changed']];
-                } else {
-                    $nodeType = is_array($currentNode1[$key]) ? 'listNode' : 'leafNode';
-                    return [...$result, $key => ['type' => $nodeType, 'value' => $currentNode1[$key], 'status' =>'unchanged']];
+//                    print_r('');
+                    return [...$result, $key => ['key' => $key, 'status' => 'added', 'node' => ['value' => $currentNode2[$key]]]];
                 }
+                if (!array_key_exists($key, $currentNode2)) {
+//                    print_r('');
+                    return [...$result, $key => ['key' => $key, 'status' => 'deleted', 'node' => ['value' => $currentNode1[$key]]]];
+                }
+                if (is_array($currentNode1[$key]) && is_array($currentNode2[$key])) {
+//                    print_r('');
+                    return [...$result, $key => ['key' => $key, 'status' => 'nested', 'node' => ['value' => $key, 'children' => $iterAst($currentNode1[$key], $currentNode2[$key])]]];
+                }
+                if ($currentNode1[$key] !== $currentNode2[$key]) {
+//                    print_r('');
+//                    $state = $nodeType === 'leafNode' ? [$currentNode1[$key], $currentNode2[$key]] : $iterAst($currentNode1[$key], $currentNode2[$key]);
+                    return [...$result,
+                        $key =>
+                            ['key' => $key,
+                                'status' => 'changed',
+                                'node' =>
+                                [
+                                    'value' => $currentNode2[$key],
+                                    'valueBeforeChange' => $currentNode1[$key],
+                                    'valueAfterChange' => $currentNode2[$key]
+                                ]
+                            ]
+
+                    ];
+                }
+//                print_r($key);
+                return [...$result, $key => ['key' => $key, 'status' =>'unchanged', 'node' => ['value' => $currentNode1[$key]]]];
+
 
             },
             $sortedKeys,
@@ -73,6 +68,7 @@ function buildDiff($firstPath, $secondPath,)
     };
 
 //    var_dump(json_encode($my($parsedNodes1, $parsedNodes2)));
-//    return $my($parsedNodes1, $parsedNodes2);
-    return buildStyledFormat($my($parsedNodes1, $parsedNodes2));
+    var_dump(buildStyledFormat($iterAst($parsedNodes1, $parsedNodes2)));
+    return buildStyledFormat($iterAst($parsedNodes1, $parsedNodes2));
+
 }
--- src/Formatters/JSONFormatter.php
+++ src/Formatters/JSONFormatter.php
@@ -1,81 +1,93 @@
 <?php
 
-//use function Differ\Formatters\JSONFormatter;
 namespace Differ\Formatters\JSONFormatter;
 
 use function Functional\flatten;
 
+const INDENT_SYMBOL = ' ';
+const INITIAL_INDENT_SIZE = 2;
+const EXTRA_INDENT_SIZE = 4;
+
 function buildStyledFormat($data)
 {
-    print_r($data);
-    $flatten = array_merge(...$data);
-    print_r($flatten);
-
-    $iter = function ($currentData, $depth) use (&$iter, ) {
-        print_r($currentData);
-
-
-        if (!is_array($currentData)) {
-            $my = trim(var_export($currentData, true), "'");
-            print_r($my);
-            return trim(var_export($currentData, true), "'");
-        }
-
-//        print_r($currentData);
-//        $otherFlatten = array_merge($currentData);
-//        print_r($otherFlatten);
-
-        $indentSize = $depth;
-        $currentIndent = str_repeat(' ', $indentSize);
-        $bracketIndent = str_repeat(' ', $indentSize - 1);
-
-        $lines = array_map(function ($key, $value) use ($currentData, $iter, $currentIndent, $depth) {
-            print_r($value);
-            if (is_string($value)) {
-                return "{$currentIndent}{$key}: {$value}";
-            }
-//                $currKey = key($value);
-            if (!is_array($value['value'])) {
-                print_r('');
-                return "{$currentIndent}{$key}: {$value['value']}";
-            }
-            if (count($value['value']) === 2 && $value['status'] === 'changed') {
-                print_r(count($value));
-                return "{$currentIndent}{$key}: {$iter($value['value'][0], $depth + 1)}\n {$currentIndent}{$key}: {$iter($value['value'][1], $depth + 1)}";
-            } elseif (count($value['value']) === 1) {
-                print_r('');
-                $vl = flatten($value['value'])[0];
-                return "{$currentIndent}{$key}: {$vl}";
-            } else {
-                print_r('');
-                $fl = array_merge(...$value['value']);
-            }
-
-//            elseif () {fl = array_merge($value['value']);}
-
-//            print_r($value['value']);
-//            $fl = '';
-//            if (count($value['value']) === 1) {
-//                $k = key($value['value']);
-//                $v = current($value['value']);
-//                print_r('');
-//
-////                return "{$currentIndent}{$k}: {$v}";
-//
-//                return "{$currentIndent}{$k}: {$v}";
-
-
-            print_r('');
-            return "{$currentIndent}{$key}: {$iter($fl, $depth + 1)}";
-        }, array_keys($currentData), $currentData);
-
-        $result = ['{', ...$lines, "{$bracketIndent}}"];
-
-        return implode("\n", $result);
+    $addIndent = function ($depth, $initial) {
+        return str_repeat(INDENT_SYMBOL, $initial + $depth * EXTRA_INDENT_SIZE);
     };
 
-    return $iter($flatten, 1);
+    $renderInnerValues = function ($value, $depth) use (&$renderInnerValues, $addIndent) {
+        return array_reduce(array_keys($value), function ($acc, $key) use ($renderInnerValues, $addIndent, $value, $depth) {
+            $indent = $addIndent($depth, INITIAL_INDENT_SIZE);
+            $indentKey = "{$indent}  {$key}";
+            return is_array($value[$key])
+                ? [$acc, "{$indentKey}: {", $renderInnerValues($value[$key], $depth + 1), "  {$indent}}"]
+                : [$acc, "{$indentKey}: {$value[$key]}"];
+        }, []);
+    };
+
+    $stringify = function ($key, $value, $depth) use ($addIndent, $renderInnerValues) {
+        $indent = $addIndent($depth, INITIAL_INDENT_SIZE);
+        if (is_array($value)) {
+            return ["{$key}: {", $renderInnerValues($value, $depth + 1), "  {$indent}}"];
+        }
+        if ('' === $value) {
+            return "{$key}:";
+        }
+        if (is_null($value)) {
+            return "{$key}: null";
+        }
+        if (is_bool($value)) {
+            $correctValue = trim(var_export($value, true), "'");
+            return "{$key}: {$correctValue}";
+        }
+        return "{$key}: {$value}";
+    };
+
+    $statusTree = [
+        'added' => function ($node, $indent, $depth) use ($stringify) {
+            [key($node) => ['key' => $key, 'node' => ['value' => $value]]] = $node;
+            $plusKey = "{$indent}+ {$key}";
+            print_r($node);
+            return $stringify($plusKey, $value, $depth);
+        },
+        'deleted' => function ($node, $indent, $depth) use ($stringify) {
+            print_r('');
+            [key($node) => ['key' => $key, 'node' => ['value' => $value]]] = $node;
+            $minusKey = "{$indent}- {$key}";
+            return $stringify($minusKey, $value, $depth);
+        },
+        'nested' => function ($node, $indent, $depth, $iterRender) use ($stringify) {
+            [key($node) => ['key' => $key, 'node' => ['value' => $value, 'children' => $children]]] = $node;
+            $emptyKey = "{$indent}  {$key}";
+            print_r($node);
+            return ["{$emptyKey}: {", $iterRender($children, $depth + 1), "  {$indent}}"];
+        },
+        'changed' => function ($node, $indent, $depth) use ($stringify) {
+            [key($node) => ['key' => $key, 'node' => ['value' => $value, 'valueBeforeChange' => $valueBefore, 'valueAfterChange' => $valueAfter]]] = $node;
+            $plusKey = "{$indent}+ {$key}";
+            $minusKey = "{$indent}- {$key}";
+            print_r($node);
+            return [$stringify($minusKey, $valueBefore, $depth), $stringify($plusKey, $valueAfter, $depth)];
+        },
+        'unchanged' => function ($node, $indent, $depth) use ($stringify) {
+            [key($node) => ['key' => $key, 'node' => ['value' => $value]]] = $node;
+            $emptyKey = "{$indent}  {$key}";
+            print_r($node);
+            return $stringify($emptyKey, $value, $depth);
+        }
+    ];
+
+    $renderDiff = function ($data, $depth = 0) use (&$renderDiff, $statusTree, $addIndent)
+    {
+        $lines = array_map(function ($node) use ($renderDiff, $depth, $statusTree, $addIndent) {
+            [key($node) => ['status' => $status]] = $node;
+            $diffTypeHandler = $statusTree[$status];
+            $indent = $addIndent($depth, INITIAL_INDENT_SIZE);
+            return $diffTypeHandler($node, $indent, $depth, $renderDiff);
+        }, $data);
+
+        $styled = implode("\n", flatten($lines));
+        return $depth === 0 ? "{\n{$styled}\n}" : $styled;
+    };
+
+    return $renderDiff($data, 0);
 }
-
-
-//return trim(var_export($value, true), "'");
